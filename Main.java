8e44e6a0342f8356e2d4f7bdfe86554a8f6669b8
zoro-22 <137077280+zoro-22@users.noreply.github.com>
2024-11-21 18:34:53 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins Pipeline!");
         System.out.println("This is a simple Java program.");
-        System.out.println("Making Chnages to trigger build")
     }
 }
